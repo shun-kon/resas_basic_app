d12f9d64801514b8c51367e016739ee3fa9ac18e
[shun-kon] <[kondo.s.ak2@gmail.com]>
2023-11-30 14:19:56 +0900

    REASIS　APIキーを隠した

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter RESAS APP',
-            localizationsDelegates: [
+      localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
